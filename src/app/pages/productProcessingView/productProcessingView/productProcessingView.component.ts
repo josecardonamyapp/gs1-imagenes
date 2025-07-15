10af61ecbfbe67744c8d5f3994c2a35fe3c2026b
Wilmer Cumes <wilmer.cumes@myappsoftware.com>
2025-07-15 12:07:54 -0600

    changes

--- src/app/pages/productProcessingView/productProcessingView/productProcessingView.component.ts
+++ src/app/pages/productProcessingView/productProcessingView/productProcessingView.component.ts
@@ -12,6 +12,8 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatDialog } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { JobConfirmationComponent } from '../../productOne/job-confirmation/job-confirmation.component';
+import { fetchUserAttributes, getCurrentUser } from 'aws-amplify/auth';
 
 @Component({
     selector: 'app-productOne',
@@ -70,12 +72,15 @@ export class productProcessingViewComponent {
     }
 
     async getPrductByGtin() {
+        const attributes = await fetchUserAttributes();
+
         this.productService.productGetByGtin(this.selectedGtin).subscribe({
             next: (result) => {
                 if (typeof (result) === 'object') {
 
                     result.data.entities.attributes.map((element: any) => {
                         const obj = {
+                            gln: attributes['custom:userOwnershipData'],
                             gtin: element.gtin,
                             producName: element.tradeitemdescriptioninformation.descriptionshort,
                             images: (Array.isArray(element.referencedfileheader)) ? element.referencedfileheader : [],
@@ -129,30 +134,123 @@ export class productProcessingViewComponent {
         };
     }
 
-    processImg() {
-        // this.isGenerating = true;
-        // const params = {
-        //     image_url: this.selectedImage,
-        //     channel_params: this.selectedChannel
-        // }
-        // this.productService.productProcessImg(params).subscribe({
-        //     next: (result: any) => {
-        //         this.isGenerating = false;
-        //         if (typeof (result) === 'object') {
-        //             // this.dialog.open(ProcessResultComponent, {
-        //             //     data: JSON.parse(result.body),
-        //             //     width: '400px'
-        //             // });
-        //         }
-        //     },
-        //     error: (error) => {
-        //         this.isGenerating = false;
-        //         console.error('Error in processImg:', error);
-        //     },
-        //     complete: () => {
-        //         this.isGenerating = false;
-        //         console.log('processImg finished.');
-        //     }
-        // })
+    sendToProcess() {
+        this.products.forEach((product) => {
+            product.images = product.images.slice(0, this.imagesPerGtin);
+            console.log('process de tamaño', product)
+
+            this.processImg(product);
+        });
+    }
+
+    processImg(product: any) {
+        this.isGenerating = true;
+        const params = {
+            images_url: product,
+            channel_params: this.selectedChannel
+        }
+
+        //(params)
+
+        this.productService.productProcessImg(params).subscribe({
+            next: (result: any) => {
+                //(result)
+                this.isGenerating = false;
+
+                // Almacenar el job_id en localStorage
+                if (result && result.job_id) {
+                    const storedJobs = localStorage.getItem('processing_jobs');
+                    const jobIds = storedJobs ? JSON.parse(storedJobs) : [];
+
+                    if (!jobIds.includes(result.job_id)) {
+                        jobIds.push(result.job_id);
+                        localStorage.setItem('processing_jobs', JSON.stringify(jobIds));
+                    }
+                }
+
+                // Crear modal de confirmación para ir a ver los jobs
+                const dialogRef = this.dialog.open(JobConfirmationComponent, {
+                    data: result,
+                    width: '500px',
+                    disableClose: true
+                });
+
+                // Manejar la respuesta del modal
+                dialogRef.afterClosed().subscribe(shouldRedirect => {
+                    if (shouldRedirect) {
+                        this.router.navigate(['/jobs']);
+                    }
+                });
+
+            },
+            error: (error) => {
+                this.isGenerating = false;
+                console.error('Error in processImg:', error);
+            },
+            complete: () => {
+                this.isGenerating = false;
+                //('processImg finished.');
+            }
+        })
+
+    }
+
+    sendToProcessNoBackground() {
+        this.products.forEach((product) => {
+            product.images = product.images.slice(0, this.imagesPerGtin);
+            console.log('process sin fondo', product)
+
+            this.processImgNoBackground(product);
+        });
+    }
+
+    processImgNoBackground(product: any) {
+        this.isGenerating = true;
+        const params = {
+            images_url: product,
+            channel_params: this.selectedChannel,
+            no_background: true
+        }
+
+        this.productService.productProcessImg(params).subscribe({
+            next: (result: any) => {
+                //(result)
+                this.isGenerating = false;
+
+                // Almacenar el job_id en localStorage
+                if (result && result.job_id) {
+                    const storedJobs = localStorage.getItem('processing_jobs');
+                    const jobIds = storedJobs ? JSON.parse(storedJobs) : [];
+
+                    if (!jobIds.includes(result.job_id)) {
+                        jobIds.push(result.job_id);
+                        localStorage.setItem('processing_jobs', JSON.stringify(jobIds));
+                    }
+                }
+
+                // Crear modal de confirmación para ir a ver los jobs
+                const dialogRef = this.dialog.open(JobConfirmationComponent, {
+                    data: result,
+                    width: '500px',
+                    disableClose: true
+                });
+
+                // Manejar la respuesta del modal
+                dialogRef.afterClosed().subscribe(shouldRedirect => {
+                    if (shouldRedirect) {
+                        this.router.navigate(['/jobs']);
+                    }
+                });
+
+            },
+            error: (error) => {
+                this.isGenerating = false;
+                console.error('Error in processImg:', error);
+            },
+            complete: () => {
+                this.isGenerating = false;
+                //('processImg finished.');
+            }
+        })
     }
 }
---
+++ src/enviroments/enviroments.prod.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: false,
+  cognitoUserPoolId: 'us-east-1_gH4j6JXGp',
+  cognitoAppClientId: '2tpnahcifdd13tovle9in0t0o5',
+  // apiUrl: 'https://zfpeurj2g7.execute-api.us-east-1.amazonaws.com/dev/',
+  // domain: 'https://d38r2sovtqprvd.cloudfront.net/',
+  identityPoolId: 'us-east-1:92da8424-ad78-4843-852e-fa7183742e15',
+  cognitoDomain: 'gs1-imagenes.auth.us-east-1.amazoncognito.com',
+  cognitoRedirectSignIn: 'https://d2kl9s2ao8r880.cloudfront.net',
+  cognitoRedirectSignOut: 'https://d2kl9s2ao8r880.cloudfront.net',
+  cognitoResponseType: 'code',
+  apicadenas: 'http://apisync.syncfonia.com/',
+  api:"https://oz0338cueg.execute-api.us-east-1.amazonaws.com/prod/",
+  domainUrl: "https://d2kl9s2ao8r880.cloudfront.net"
+  // apiregistry: 'https://6d6j5r4cdl.execute-api.us-east-1.amazonaws.com/',
+}
+
